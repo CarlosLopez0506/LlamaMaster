9524912438bd91597d24770835e486cb1c1f34e2
Braulio Solorio <soloriobraulio03@gmail.com>
2023-11-08 08:32:19 -0600

    Main Menu Scene

---
+++ Assets/UI/Scripts/ColorSwitch.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+public class ColorSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private Color textHoverColor = new Color(255, 255, 255); 
+    public GameObject button;
+    // Start is called before the first frame update
+    void SwapColor(){
+
+    }
+}
