f9eea08dc79d01c30f599d35db362c40e6ef6ada
DJ2513 <djontiveros2728@gmail.com>
2023-11-09 19:52:33 -0600

    Mi primer commit

---
+++ Assets/Scripts/Control/Saludar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saludar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola estoy saludando!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
